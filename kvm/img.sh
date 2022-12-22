cbc8c4ac77a478691127e299cc1877ba33670075
Rune Morling <ermo@serpentos.com>
2022-12-22 16:27:09 +0100

    img.sh: Initial productisation cleanup
    - Add license headers for compliance
    - Check that script is run from correct directory up front
    - Check for necessary tools up front
    - Be verbose and forceful about cleanups and moves/copies
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- kvm/img.sh
+++ kvm/img.sh
@@ -1,66 +1,126 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: © 2020-2022 Serpent OS Developers
+#
+# SPDX-License-Identifier: Zlib
+#
+# Serpent OS prototype linux-kvm ISO image generator
 
-# Pkg list
+die () {
+    echo -e "$*"
+    exit 1
+}
+
+# Add escape codes for color
+RED='\033[0;31m'
+RESET='\033[0m'
+
+# Pkg list check
+test -f ./pkglist || die "\nThis script MUST be run from within the kvm/ dir with the ./pkglist file.\n"
 pkgs=$(cat pkglist)
 
 # Root check
 if [[ "${UID}" -ne 0 ]]; then
-    echo "This script MUST be run as root"
-    exit 1
+    die "\nThis script MUST be run as root.\n"
 fi
 
-test -d mount && rmdir mount
-test -d root && rmdir root
-test -e rootfs.img && rm rootfs.img
-test -d LiveOS && rm -rf LiveOS
-test -d boot && rm -rf boot
+BINARIES=(
+    e2fsck
+    fallocate
+    mkfs.ext3
+    mkfs.vfat
+    moss
+    moss-container
+    mount
+    resize2fs
+    sync
+    systemctl
+    xorriso
+)
+# up front check for necessary binaries
+BINARY_NOT_FOUND=0
+echo -e "\nChecking for necessary prerequisites..."
+# 'all entries in the BINARIES array'
+for b in ${BINARIES[@]}; do
+    command -v ${b} > /dev/null 2>&1
+    if [[ ! ${?} -eq 0 ]]; then
+        echo -e "- ${b} ${RED}not found${RESET} in \$PATH."
+        BINARY_NOT_FOUND=1
+    else
+        echo "- found ${b}"
+    fi
+done
+
+if [[ ${BINARY_NOT_FOUND} -gt 0 ]]; then
+    die "\nNecessary prerequisites not met, please install missing tool(s).\n"
+else
+    echo -e "\nAll necessary binaries found, generating Serpent OS linux-kvm ISO image...\n"
+fi
+#die "Exit because this is just a test."
+
+# From here on, exit from script on any non-zero exit status command result
+set -e
+
+DIRS=(
+    mount
+    root
+    LiveOS
+    boot
+)
+
+# clean up dirs
+for d in ${DIRS[@]}; do
+    test -d ${d} && rm -rf ${d}
+done
+# clean up existing rootfs.img
+test -e rootfs.img && rm -f rootfs.img
 
 # Stash boot assets
-mkdir boot
+mkdir -pv boot
 
 # Get it right first time.
-mkdir mount
-chown root:root mount
-chmod 00755 mount
+mkdir -pv mount
+chown -Rc root:root mount
+chmod -Rc 00755 mount
 
 # Setup the root image
 fallocate -l 4GB rootfs.img
+# don't want/need journaling on the fs
 mkfs.ext3 -F rootfs.img
 mount -o loop rootfs.img mount
 
 # Add repositories
-moss -D mount ar protosnek -p 0 https://dev.serpentos.com/protosnek/x86_64/stone.index
-moss -D mount ar volatile -p 10 https://dev.serpentos.com/volatile/x86_64/stone.index
+moss -D mount/ ar protosnek -p 0 https://dev.serpentos.com/protosnek/x86_64/stone.index
+moss -D mount/ ar volatile -p 10 https://dev.serpentos.com/volatile/x86_64/stone.index
 
 # Install the pkgs
-moss -D mount it -y $pkgs
+moss -D mount/ it -y $pkgs
 
 # Fix ldconfig
-mkdir mount/var/cache/ldconfig -p
-moss-container -u 0 -d mount -- ldconfig
+mkdir -pv mount/var/cache/ldconfig
+moss-container -u 0 -d mount/ -- ldconfig
 
 # Get basic env working
-moss-container -u 0 -d mount -- systemd-sysusers
-moss-container -u 0 -d mount -- systemd-tmpfiles --create
-moss-container -u 0 -d mount -- systemd-firstboot --force --setup-machine-id --delete-root-password --locale=en_US.UTF-8 --timezone=UTC --root-shell=/usr/bin/bash
-moss-container -u 0 -d mount -- systemctl enable systemd-resolved systemd-networkd getty@tty1
+moss-container -u 0 -d mount/ -- systemd-sysusers
+moss-container -u 0 -d mount/ -- systemd-tmpfiles --create
+moss-container -u 0 -d mount/ -- systemd-firstboot --force --setup-machine-id --delete-root-password --locale=en_US.UTF-8 --timezone=UTC --root-shell=/usr/bin/bash
+moss-container -u 0 -d mount/ -- systemctl enable systemd-resolved systemd-networkd getty@tty1
 
 # TODO: Install dracut, rebuild the initrd from the "current" kernel
 # Extract assets
-cp mount/usr/lib/systemd/boot/efi/systemd-bootx64.efi boot/bootx64.efi
-cp mount/usr/lib/kernel/com.serpentos.* boot/kernel
+cp -v mount/usr/lib/systemd/boot/efi/systemd-bootx64.efi boot/bootx64.efi
+cp -v mount/usr/lib/kernel/com.serpentos.* boot/kernel
 
 # Regenerate dracut. BLUH.
 kver=$(ls mount/usr/lib/modules)
-moss-container -u 0 -d mount -- dracut --hardlink -N --nomdadmconf --nolvmconf --kver ${kver} --add "bash dash systemd lvm dm dmsquash-live" --fwdir /usr/lib/firmware --tmpdir /tmp --zstd /initrd
-cp mount/initrd boot/initrd
+moss-container -u 0 -d mount/ -- dracut --hardlink -N --nomdadmconf --nolvmconf --kver ${kver} --add "bash dash systemd lvm dm dmsquash-live" --fwdir /usr/lib/firmware --tmpdir /tmp --zstd /initrd
+cp -v mount/initrd boot/initrd
 
 # Cleanup!
 rm -rf mount/.moss/cache/downloads/*
 
 # Tear it down
-sudo umount $(pwd)/mount
+umount $(pwd)/mount
 
 # Shrink size to minimum
 resize2fs -M rootfs.img -f
@@ -70,14 +130,14 @@ e2fsck -fy rootfs.img
 
 # Now gen the structure
 
-mkdir LiveOS
-mv rootfs.img LiveOS/.
+mkdir -pv LiveOS
+mv -v rootfs.img LiveOS/.
 mksquashfs LiveOS/ squashfs.img -comp xz -root-becomes LiveOS -keep-as-directory -all-root
-rm LiveOS/rootfs.img
-mv squashfs.img LiveOS/.
+rm -f LiveOS/rootfs.img
+mv -v squashfs.img LiveOS/.
 
-mkdir root
-mv LiveOS root/.
+mkdir -pv root
+mv -v LiveOS root/.
 
 # Create the efi img
 fallocate -l 25M efi.img
@@ -85,18 +145,18 @@ mkfs.vfat -F 12 efi.img -n EFIBOOTISO
 mount -o loop efi.img mount
 
 # Set it up...
-mkdir -p mount/EFI/Boot
-cp -a boot/bootx64.efi mount/EFI/Boot/bootx64.efi
+mkdir -pv mount/EFI/Boot
+cp -v boot/bootx64.efi mount/EFI/Boot/bootx64.efi
 sync
-mkdir -p mount/loader/entries
-cp live-os.conf mount/loader/entries/.
-cp boot/kernel mount/kernel
-cp boot/initrd mount/initrd
+mkdir -pv mount/loader/entries
+cp -v live-os.conf mount/loader/entries/.
+cp -v boot/kernel mount/kernel
+cp -v boot/initrd mount/initrd
 umount $(pwd)/mount
 
 # Put it in place
-mkdir -p root/EFI/Boot
-mv efi.img root/EFI/Boot/efiboot.img
+mkdir -pv root/EFI/Boot
+mv -v efi.img root/EFI/Boot/efiboot.img
 
 # Create the ISO
 xorriso -as mkisofs \
