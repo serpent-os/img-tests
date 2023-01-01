7bf103921f46f712f162febe2a7cd1cc661b6a2c
Ikey Doherty <ikey@serpentos.com>
2023-01-01 12:19:07 +0000

    Update copyright ranges
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- kvm/img.sh
+++ kvm/img.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# SPDX-FileCopyrightText: © 2020-2022 Serpent OS Developers
+# SPDX-FileCopyrightText: © 2020-2023 Serpent OS Developers
 #
 # SPDX-License-Identifier: Zlib
 #
