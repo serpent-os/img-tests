a4b06a809c1a71706c478f09777c77fac402fb5a
Ikey Doherty <ikey@serpentos.com>
2023-11-22 11:07:03 +0000

    kvm/diskImg: Prepare as we would the live ISO, fixing UEFI console issues
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- kvm/diskImg.sh
+++ kvm/diskImg.sh
@@ -16,7 +16,7 @@ mkpart "EFI System Partition" fat32 500MiB 600MiB
 mkpart "Serpent OS Root Partition" ext2 600MiB 100%
 set 2 esp on
 set 1 bls_boot on
-type-uuid 3 "4f68bce3-e8cd-4db1-96e7-fbcaf984b709"
+type 3 "4f68bce3-e8cd-4db1-96e7-fbcaf984b709"
 EOF
 LODEVICE=$(losetup -f disk.img --show -P)
 
@@ -41,8 +41,21 @@ echo "Loopback is at ${LODEVICE}"
 sync
 
 # get moss in.
-moss -D root/ ar volatile https://dev.serpentos.com/volatile/x86_64/stone.index
-moss -D root it $(cat pkglist) -y
+moss -D root/ repo add volatile https://dev.serpentos.com/volatile/x86_64/stone.index
+moss -D root install $(cat pkglist) -y
+
+# Fix ldconfig
+mkdir -pv root/var/cache/ldconfig
+moss-container -u 0 -d root/ -- ldconfig
+
+# Get basic env working
+moss-container -u 0 -d root/ -- systemd-sysusers
+moss-container -u 0 -d root/ -- systemd-tmpfiles --create
+moss-container -u 0 -d root/ -- systemd-firstboot --force --setup-machine-id --delete-root-password --locale=en_US.UTF-8 --timezone=UTC --root-shell=/usr/bin/bash
+moss-container -u 0 -d root/ -- systemctl enable systemd-resolved systemd-networkd getty@tty1
+
+# Fix perf issues. Needs packaging/merging by moss
+moss-container -u 0 -d root/ -- systemd-hwdb update
 
 # OS kernel assets
 mkdir root/boot/com.serpentos
