1f72afade5b09ae66c246ef3f51bf6f00a2924cd
Rune Morling <ermo@serpentos.com>
2024-02-14 21:55:40 +0100

    nspawn and virt-manager consolidation and tweaks
    - create-virtio-vm.sh script now uses UEFI instaed of BIOS
    - basic-setup.sh script with shared setup and cleanup functions added
    - basic-setup.sh script sets up an empty root passwd by default
    - basic-setup.sh sets up a nss-systemd compatible /etc/nsswitch.conf ootb
    - create-virtio-vm.sh and create-sosroot.sh scripts uses basic-setup.sh
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

---
+++ basic-setup.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# Copyright: © 2023-2024 Serpent OS Developers
+#
+
+# shared-setup.sh:
+# script with shared utility functions for conveniently creating a
+# clean serpent-os root directory  directory suitable for use as the
+# root in serpent os systemd-nspawn container or linux-kvm kernel driven
+# qemu-kvm virtual machine.
+
+# target dirs
+# use a default sosroot
+SOSROOT="${DESTDIR:-./sosroot}"
+BOULDERCACHE="/var/cache/boulder"
+
+# utility functions
+BOLD='\033[1m'
+RED='\033[0;31m'
+RESET='\033[0m'
+YELLOW='\033[0;33m'
+
+printInfo () {
+    local INFO="${BOLD}INFO${RESET}"
+    echo -e "${INFO} ${*}"
+}
+
+printWarning () {
+    local WARNING="${YELLOW}${BOLD}WARNING${RESET}"
+    echo -e "${WARNING} ${*}"
+}
+
+printError () {
+    local ERROR="${RED}${BOLD}ERROR${RESET}"
+    echo -e "${ERROR} ${*}"
+}
+
+die() {
+    printError "${*} failed, exiting.\n"
+    exit 1
+}
+
+checkPrereqs () {
+    # prerequisite checks
+    test -f ./pkglist-base || die "\nRun this script from the root of the img-tests/ repo clone!\n"
+    test -x /usr/bin/moss || die "\n${0} assumes moss is installed. See https://github.com/serpent-os/onboarding/\n"
+}
+
+# base packages
+readarray -t PACKAGES < ./pkglist-base
+
+#echo "${PACKAGES[@]}"
+#die "Test of PACKAGES."
+
+createNssswitchConf () {
+    cat << EOF > ./nsswitch.conf
+passwd:         files systemd
+group:          files [SUCCESS=merge] systemd
+shadow:         files systemd
+gshadow:        files systemd
+
+hosts:          mymachines resolve [!UNAVAIL=return] files myhostname dns
+networks:       files
+
+protocols:      db files
+services:       db files
+ethers:         db files
+rpc:            db files
+EOF
+}
+
+basicSetup () {
+    MSG="Removing old ${SOSROOT} directory..."
+    printInfo "${MSG}"
+    sudo rm -rf "${SOSROOT}" || die "${MSG}"
+
+    MSG="Creating new ${SOSROOT} directory w/baselayout skeleton..."
+    printInfo "${MSG}"
+    sudo mkdir -pv "${SOSROOT}"/{etc,proc,run,sys,var,var/local} || die "${MSG}"
+
+    MSG="Ensuring that we get a working nss-systemd-compatible nssswitch.conf..."
+    printInfo "${MSG}"
+    createNssswitchConf || die "${MSG}"
+    sudo cp -v ./nsswitch.conf "${SOSROOT}"/etc/ || die "${MSG}"
+
+    MSG="Ensuring that various network protocols function..."
+    printInfo "${MSG}"
+    sudo cp -va /etc/protocols "${SOSROOT}"/etc/ || die "${MSG}"
+
+    MSG="Adding volatile serpent os repository..."
+    printInfo "${MSG}"
+    sudo moss -D "${SOSROOT}" repo add volatile https://dev.serpentos.com/volatile/x86_64/stone.index -p0 || die "${MSG}"
+
+    MSG="Installing packages..."
+    printInfo "${MSG}"
+    sudo moss -D "${SOSROOT}" install "${PACKAGES[@]}" || die "${MSG}"
+
+    MSG="Setting up an empty root password by default..."
+    printInfo "${MSG}"
+    sudo chroot "${SOSROOT}" /usr/bin/passwd -d root
+    sudo rm -vf issue
+    cp -v "${SOSROOT}"/etc/issue issue
+    echo -e "By default, the root user has no password.\n\nUse the passwd command to change it.\n" >> issue
+    sudo cp -v issue "${SOSROOT}"/etc/issue
+
+    MSG="Preparing local-x86_64 profile directory..."
+    printInfo "${MSG}"
+    sudo mkdir -pv ${BOULDERCACHE}/repos/local-x86_64/ || die "${MSG}"
+
+    MSG="Creating a moss stone.index file for the local-x86_64 profile..."
+    printInfo "${MSG}"
+    sudo moss index ${BOULDERCACHE}/repos/local-x86_64/ || die "${MSG}"
+
+    MSG="Adding local-x86_64 profile to list of active repositories..."
+    printInfo "${MSG}"
+    sudo moss -D "${SOSROOT}" repo add local-x86_64 file://${BOULDERCACHE}/repos/local-x86_64/stone.index -p10 || die "${MSG}"
+}
+
+# clean up env
+cleanEnv () {
+    unset BOULDERCACHE
+    unset MSG
+    unset PACKAGES
+    unset SOSROOT
+
+    unset BOLD
+    unset RED
+    unset RESET
+    unset YELLOW
+}
--- create-sosroot.sh
+++ create-sosroot.sh
@@ -10,46 +10,7 @@
 # directory suitable for use as the root in serpent os systemd-nspawn
 # container or linux-kvm kernel driven qemu-kvm virtual machine.
 
-# target dirs
-# use a default sosroot
-SOSROOT="${DESTDIR:-./sosroot}"
-BOULDERCACHE="/var/cache/boulder"
-
-# utility functions
-BOLD='\033[1m'
-RED='\033[0;31m'
-RESET='\033[0m'
-YELLOW='\033[0;33m'
-
-printInfo () {
-    local INFO="${BOLD}INFO${RESET}"
-    echo -e "${INFO} ${*}"
-}
-
-printWarning () {
-    local WARNING="${YELLOW}${BOLD}WARNING${RESET}"
-    echo -e "${WARNING} ${*}"
-}
-
-printError () {
-    local ERROR="${RED}${BOLD}ERROR${RESET}"
-    echo -e "${ERROR} ${*}"
-}
-
-die() {
-    printError "${*}\n"
-    exit 1
-}
-
-# prerequisite checks
-test -f ./pkglist-base || die "\nRun this script from the root of the img-tests/ repo clone!\n"
-test -x /usr/bin/moss || die "\n${0} assumes moss is installed. See https://github.com/serpent-os/onboarding/\n"
-
-# base packages
-readarray -t PACKAGES < ./pkglist-base
-
-#echo "${PACKAGES[@]}"
-#die "Test of PACKAGES."
+source ./basic-setup.sh
 
 showHelp() {
     cat <<EOF
@@ -85,53 +46,9 @@ exec sudo systemd-nspawn --bind=${BOULDERCACHE}/ -D ${SOSROOT}/ -b
 EOF
 }
 
-MSG="Removing old ${SOSROOT} directory..."
-printInfo "${MSG}"
-sudo rm -rf "${SOSROOT}" || die "${MSG} failed, exiting."
-
-MSG="Creating new ${SOSROOT} directory..."
-printInfo "${MSG}"
-sudo mkdir -pv "${SOSROOT}" || die "${MSG} failed, exiting."
-
-MSG="Adding volatile serpent os repository..."
-printInfo "${MSG}"
-sudo moss -D "${SOSROOT}" repo add volatile https://dev.serpentos.com/volatile/x86_64/stone.index -p0 || die "${MSG} failed, exiting."
-
-MSG="Installing packages..."
-printInfo "${MSG}"
-sudo moss -D "${SOSROOT}" install "${PACKAGES[@]}"
-
-MSG="Preparing local-x86_64 profile directory..."
-printInfo "${MSG}"
-sudo mkdir -pv ${BOULDERCACHE}/repos/local-x86_64/ || die "${MSG} failed, exiting."
-
-MSG="Creating a moss stone.index file for the local-x86_64 profile..."
-printInfo "${MSG}"
-sudo moss index ${BOULDERCACHE}/repos/local-x86_64/ || die "${MSG} failed, exiting."
-
-MSG="Adding local-x86_64 profile to list of active repositories..."
-printInfo "${MSG}"
-sudo moss -D "${SOSROOT}" repo add local-x86_64 file://${BOULDERCACHE}/repos/local-x86_64/stone.index -p10 || die "${MSG} failed, exiting."
-
-MSG="Ensuring that an /etc directory exists in ${SOSROOT}..."
-printInfo "${MSG}"
-sudo mkdir -pv "${SOSROOT}"/etc/ || die "${MSG} failed, exiting."
-
-MSG="Ensuring that various network protocols function..."
-printInfo "${MSG}"
-sudo cp -va /etc/protocols "${SOSROOT}"/etc/ || die "${MSG} failed, exiting."
-
+checkPrereqs
+basicSetup
 showHelp
 # Make it simple to boot into the created sosroot at a later point
 createBootScript
-
-# clean up env
-unset BOULDERCACHE
-unset MSG
-unset PACKAGES
-unset SOSROOT
-
-unset BOLD
-unset RED
-unset RESET
-unset YELLOW
+cleanEnv
